2ffcd4d8d96c1bcc6e17d106e2761eeb4ab39ff6
wendermendes <105708282+wendermendes@users.noreply.github.com>
2022-12-05 12:28:21 -0300

    Update dependencias.sh

--- dependencias.sh
+++ dependencias.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
+#ATUALIZAÇÃO DO SO
 sudo apt-get update
 sudo apt-get install -y apt-utils
-
 sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
+#PASSO A PASSO PARA INSTALAÇÃO DO DOCKER SEGUNDO DOCUMENTAÇÃO OFICIAL
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 sudo apt-get update
-
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
-
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-docker run --name mariadb -e MARIADB_ROOT_PASSWORD=diouxx -e MARIADB_DATABASE=glpidb -e MARIADB_USER=glpi_user -e MARIADB_PASSWORD=glpi -d mariadb:10.7
-docker run --name glpi --link mariadb:mariadb -p 80:80 -d diouxx/glpi
-
-docker run -d --name=grafana -p 3000:3000 grafana/grafana
+#CRIAÇÃO DOS CONTAINERS QUE VÃO RODAR AS APLICAÇÕES
+docker run --name tcloud_zbx_db -e MARIADB_ROOT_PASSWORD=root_password -e MARIADB_DATABASE=zabbix_db -e MARIADB_USER=zabbix_usr -e MARIADB_PASSWORD=zabbox_pwd --volume /var/lib/mysql:/var/lib/mysql -d mariadb:10.7
+docker run --name tcloud_zbx_fe --link mariadb:mariadb -p 80:80 -d truecharts/zabbix-server
+docker run --name tcloud_gfn -p 3000:3000 -d grafana/grafana
